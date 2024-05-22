5e59f296992ae755dc7f9f193eedb04bf3c3110f
radioperez <brostikslavik@yandex.com>
2024-05-22 14:49:54 +0700

    Several canvases, switching not finished

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from PyQt6.QtWidgets import (
     QLabel,
     QToolBar,
     QColorDialog,
+    QHBoxLayout,
+    QFormLayout,
+    QPushButton
 )
 from PyQt6.QtGui import (
     QAction,
@@ -38,11 +41,17 @@ class Caretaker:
         if len(self.history) == self.HISTORY_LEN: self.history = self.history[1:]
         self.history.append(memento)
     def get_last(self):
-        return self.history.pop()
+        try:
+            last = self.history.pop()
+        except IndexError:
+            return
+        return last
 
 class Canvas_widget(QWidget):
-    def __init__(self, *args, **kwargs):
+    idx = None
+    def __init__(self, idx, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.idx = idx
         self.setFixedSize(680, 480)
         self.canvas = Canvas(500,500)
         self.caretaker = Caretaker()
@@ -76,6 +85,23 @@ class Canvas_widget(QWidget):
         self.last_pos = None
         QWidget.mouseReleaseEvent(self,e)
 
+class Database:
+    index = 0
+    items = {}
+    def create(self) -> Canvas_widget:
+        self.index += 1
+        canvas = Canvas_widget(self.index)
+        self.items[self.index] = canvas
+        return canvas
+    def read(self, idx) -> Canvas_widget:
+        return self.items[idx]
+    def update(self, object) -> Canvas_widget:
+        object.canvas.state.save(f'canvas{object.idx}.png')
+        self.items[object.idx] = object
+        return self.items[object.idx]
+    def delete(self, object) -> Canvas_widget:
+        return self.items.pop(object.idx)
+
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
@@ -95,24 +121,63 @@ class MainWindow(QMainWindow):
         inc_size = QAction("+", self)
         dec_size = QAction("-", self)
         self.pen_size = 1
+        save = QAction("Save", self)
+        new = QAction("New", self)
         toolbar.addAction(self.show_size)
         toolbar.addAction(inc_size)
         toolbar.addAction(dec_size)
+        toolbar.addAction(save)
+        toolbar.addAction(new)
 
+        self.root = QWidget()
+        self.layout = QHBoxLayout()
+        self.canvas_list = QFormLayout()
+
+        self.db = Database()
         # Canvas
-        self.canvas_widget = Canvas_widget()
+        self.canvas_widget = Canvas_widget(0)
+        self.on_new()
+        self.current_index = self.canvas_widget.idx
+
         undo.triggered.connect(self.undo)
         self.get_color.triggered.connect(self.set_color)
         inc_size.triggered.connect(self.inc_size)
         dec_size.triggered.connect(self.dec_size)
+        
+        new.triggered.connect(self.on_new)
+        save.triggered.connect(lambda: self.db.update(self.canvas_widget))
 
-        self.setCentralWidget(self.canvas_widget)
+        self.layout.addWidget(self.canvas_widget)
+        self.layout.addLayout(self.canvas_list)
+        self.root.setLayout(self.layout)
+
+        self.setCentralWidget(self.root)
+
+    def on_new(self):
+        canvas = self.db.create()
+        self.set_canvas(canvas)
+        canvas_button = QPushButton(f"Canvas {canvas.idx}")
+        canvas_button.clicked.connect(lambda: self.set_canvas(self.db.read(canvas.idx)))
+        x_button = QPushButton("x")
+        x_button.clicked.connect(lambda: self.on_x(canvas_button, canvas))
+        self.canvas_list.addRow(canvas_button, x_button)
+    
+    def on_x(self, button, canvas):
+        if len(self.db.items) == 1: return
+        self.db.delete(canvas)
+        self.canvas_list.removeRow(button)
+   
     def undo(self):
         savestate = self.canvas_widget.caretaker.get_last()
         if savestate: 
             self.canvas_widget.canvas.restore_state(savestate)
             self.canvas_widget.update()
-
+    
+    def set_canvas(self, canvas: Canvas_widget):
+        self.layout.replaceWidget(self.canvas_widget, canvas)
+        self.canvas_widget = canvas
+        self.layout.replaceWidget(canvas, self.canvas_widget)
+    
     def set_color(self):
         color = QColorDialog.getColor(self.color, self)
         if color:
--- nopattern.py
+++ nopattern.py
@@ -7,6 +7,9 @@ from PyQt6.QtWidgets import (
     QLabel,
     QToolBar,
     QColorDialog,
+    QHBoxLayout,
+    QFormLayout,
+    QPushButton
 )
 from PyQt6.QtGui import (
     QAction,
@@ -18,12 +21,14 @@ from PyQt6.QtGui import (
 )
 class Canvas(QWidget):
     HISTORY_LEN = 10
-    def __init__(self, *args, **kwargs):
+    idx = None
+    def __init__(self, idx, *args, **kwargs):
         super().__init__(*args,**kwargs)
         self.setFixedSize(680, 480)
         self.canvas = QPixmap(self.size())
         self.canvas.fill(QColor("white"))
 
+        self.idx = idx
         self.last_pos = None
         self.painter = QPainter()
         self.pen = QPen(QColor("black"),1)
@@ -60,6 +65,8 @@ class Canvas(QWidget):
 
 
 class MainWindow(QMainWindow):
+    index = 0
+    canvases = {}
     def __init__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
         self.setGeometry(100, 100, 800, 600)
@@ -78,18 +85,62 @@ class MainWindow(QMainWindow):
         inc_size = QAction("+", self)
         dec_size = QAction("-", self)
         self.pen_size = 1
+        add_canvas = QAction("Add", self)
+        save_canvas = QAction("Save", self)
         toolbar.addAction(self.show_size)
         toolbar.addAction(inc_size)
         toolbar.addAction(dec_size)
+        toolbar.addAction(save_canvas)
+        toolbar.addAction(add_canvas)
+
+        save_canvas.triggered.connect(self.save)
+        add_canvas.triggered.connect(self.new_canvas)
+        layout = QHBoxLayout()
+
+        self.canvas_list = QFormLayout()
+        self.canvas_widget = None
+        self.new_canvas()
 
         # Canvas
-        self.canvas_widget = Canvas()
-        undo.triggered.connect(self.canvas_widget.restoreCanvas)
+        #self.canvas_widget = Canvas(idx= )
+        #undo.triggered.connect(self.canvas_widget.restoreCanvas)
         self.get_color.triggered.connect(self.set_color)
         inc_size.triggered.connect(self.inc_size)
         dec_size.triggered.connect(self.dec_size)
 
-        self.setCentralWidget(self.canvas_widget)
+        layout.addWidget(self.canvas_widget)
+        layout.addLayout(self.canvas_list)
+        root = QWidget()
+        root.setLayout(layout)
+        self.setCentralWidget(root)
+    def new_canvas(self):
+        self.index += 1
+        idx = self.index
+
+        self.canvases[idx] = Canvas(idx=idx)
+        self.canvas_widget = self.canvases[idx]
+        self.update()
+
+        canvas_button = QPushButton(f"Canvas {idx}")
+        canvas_button.clicked.connect(lambda: self.get_canvas(idx))
+        x_button = QPushButton("x")
+        x_button.clicked.connect(lambda: self.remove_canvas(canvas_button, idx))
+        self.canvas_list.addRow(canvas_button, x_button)
+    def remove_canvas(self, object, idx):
+        if self.index == 1: return
+        self.canvases.pop(idx)
+        self.canvas_list.removeRow(object)
+    def save(self):
+        copy = self.canvas_widget
+        self.canvases[self.canvas_widget.idx] = copy
+    def get_canvas(self, idx):
+        print(f'Previous canvas: {self.canvas_widget}, Replaced with: {self.canvases[idx]}')
+        self.canvas_widget = self.canvases[idx]
+        self.canvas_widget.update()
+        self.update()
+        print(self.canvas_widget == self.canvases[idx])
+    def ping(self):
+        print("PING")
     def set_color(self):
         color = QColorDialog.getColor(self.color, self)
         if color:
